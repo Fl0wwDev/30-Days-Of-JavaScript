6faeb69b61d86d9b32fac2e55158e3bba80899b5
Fl0wwDev <mafradin@hotmail.fr>
2024-12-02 12:35:20 +0100

    day 6 fini

--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -1,2 +1,9 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+
+
+//5
+
+for(let j=0; j<=100; j++){
+    if (j % 2 == 0){
+        console.log(j)
+    }
+}
