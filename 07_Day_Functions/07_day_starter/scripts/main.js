ea8c19d586fec46ab1dd3efc1e8459d544e0b39d
Fl0wwDev <mafradin@hotmail.fr>
2024-12-02 22:21:20 +0100

    day 8 fini

--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -1,2 +1,2 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loadeed')
+console.log(countries);
+alert('Open the console and check if the countries has been loadeed');
